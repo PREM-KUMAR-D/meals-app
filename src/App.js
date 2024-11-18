548378a90efc852d06441cc7b29be05de4a0479c
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-11-18 19:47:59 +0530

    inital setup for the meals app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/UI/Header";
+
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
+  );
+}
+
+export default App;
