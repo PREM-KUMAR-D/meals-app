30596b740ae38a815818021fd2a4b3304f35b9d0
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-11-19 07:11:50 +0530

    Cart open and close functionality completed

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -5,7 +5,7 @@ import classes from "./Modal.module.css";
 const Backdrop = (props)=>{
 
     return (
-        <div className={classes.backdrop}>
+        <div className={classes.backdrop} onClick={props.onClose}>
 
         </div>
     )
@@ -31,7 +31,7 @@ const Modal = (props)=>{
     return (
         <>
 
-            {ReactDom.createPortal(<Backdrop/>,portalElement)}
+            {ReactDom.createPortal(<Backdrop onClose={props.onClose}/>,portalElement)}
             {ReactDom.createPortal(<ModalOverlay>{props.children}</ModalOverlay>,portalElement)}
 
 
