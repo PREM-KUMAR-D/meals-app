0f9001f002a4f57b1c973c8e7563f529e8639e32
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-11-19 06:19:47 +0530

    Added  form meal item

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,23 +1,22 @@
 import React from "react";
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
-const MealItem = (props)=>{
+const MealItem = (props) => {
 
     const price = `$${props.price.toFixed(2)}`;
 
 
     return (
-        <li  className={classes.meal}>
+        <li className={classes.meal}>
 
-            <div className={classes.description}>
-                <h3>
-                    {props.title}
-                </h3>
-                {props.description}
+            <div>
+                <h3>{props.title}</h3>
+                <div className={classes.description}>{props.description}</div>
+                <div className={classes.price}>{price}</div>
             </div>
-
-            <div className={classes.price}>
-                {price}
+            <div>
+                <MealItemForm id={props.id} />
             </div>
         </li>
 
